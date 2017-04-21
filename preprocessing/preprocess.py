2e0c6b754aa74e0d2eea6482db4b60d2913d8243
Mehdi Rahim <rahim.mehdi@gmail.com>
2017-04-21 16:08:27 +0200

    preprocessing script

---
+++ preprocessing/preprocess.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from pypreprocess.nipype_preproc_spm_utils import do_subjects_preproc
+
+config = sys.argv[-1]
+_, extension = os.path.splitext(config)
+
+if extension != '.ini':
+    print('%s is not .ini file' % config)
+else:
+    # preprocess the data
+    results = do_subjects_preproc(config)
