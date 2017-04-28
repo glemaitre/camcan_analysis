884c9b88e2a284af41772f5154321920ed76c85b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-04-28 11:20:09 +0200

    CI add travis support

---
+++ build_tools/travis/install.sh
@@ -0,0 +1,36 @@
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead
+deactivate
+
+# Use the miniconda installer for faster download / install of conda
+# itself
+pushd .
+cd
+mkdir -p download
+cd download
+echo "Cached in $HOME/download :"
+ls -l
+echo
+if [[ ! -f miniconda.sh ]]
+   then
+   wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
+        -O miniconda.sh
+   fi
+chmod +x miniconda.sh && ./miniconda.sh -b
+cd ..
+export PATH=/home/travis/miniconda2/bin:$PATH
+conda update --yes conda
+popd
+
+# Configure the conda environment and put it in the path using the
+# provided versions
+conda create -n testenv --yes python=$PYTHON_VERSION pip nose
+    libgfortran nomkl
+source activate testenv
+
+# Install nose-timer via pip
+pip install nose-timer coverage codecov
+
+cd $TRAVIS_BUILD_DIR
+pip install -r requirements.txt
+python setup.py develop
