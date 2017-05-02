65ac6f0c3145d717635fbfb27c4c033a8b3c488a
Jerome Dockes <jerome@dockes.org>
2017-05-02 17:38:49 +0200

    add a loader for contrasts

--- camcan/datasets/__init__.py
+++ camcan/datasets/__init__.py
@@ -4,7 +4,9 @@ The :mod:`camcan.datasets` module includes utilities to load datasets.
 from .camcan import load_camcan_rest
 from .camcan import load_camcan_timeseries_rest
 from .camcan import load_camcan_connectivity_rest
+from .camcan import load_camcan_contrast_maps
 
 __all__ = ['load_camcan_rest',
            'load_camcan_timeseries_rest',
-           'load_camcan_connectivity_rest']
+           'load_camcan_connectivity_rest',
+           'load_camcan_contrast_maps']
